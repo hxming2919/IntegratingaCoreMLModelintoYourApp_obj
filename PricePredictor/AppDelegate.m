d53a53ddc921828c15516efad710154e6d4cf163
hxming2919 <329287241@qq.com>
2017-08-27 11:07:19 +0800

    <branch>我是nb分支，第二次提交

--- PricePredictor/AppDelegate.m
+++ PricePredictor/AppDelegate.m
@@ -28,6 +28,9 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    
+    // 我是nb分支，现在第二次提交
 }
 
 
